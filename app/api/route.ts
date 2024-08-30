1e5afda45d9c8eb79d19bae4bdf82d52fc3a43c9
DevM7mdAli <DM7A+Github@outlook.sa>
2024-08-30 21:43:53 +0300

    adding chat ability

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,11 +1,16 @@
 import model from "../googleAI"
 
+let chatHistory:any = [];
+
 export async function POST(
   req: Request,
 ) {
   const body = await req.json()
   if(body.person){
-    const aiRes = await model.generateContent(body.person);
+    const chat = model.startChat({ history : chatHistory})
+    
+    const aiRes = await chat.sendMessage(body.person)
+
     return new Response(JSON.stringify({ai: aiRes.response.text()}))
     } else {
       return new Response(JSON.stringify({ai: "I cant tell you anything without saying anything"}))
