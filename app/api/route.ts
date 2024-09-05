1a8aed1b11f1d021e1172925bbdbfa977e0043f7
DevM7mdAli <DM7A+Github@outlook.sa>
2024-09-05 18:44:01 +0300

    making each chat seperate by the ip

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,13 +1,27 @@
 import model from "../googleAI"
 
-let chatHistory:any = [];
+function getIP(ip : Request){
+    return ip.headers.get('x-forwarded-for') || ip.headers.get('remote-addr');
+}
+
+
+let chatHistories: { [ip: string]: any[] } = {};
 
 export async function POST(
   req: Request,
 ) {
   const body = await req.json()
+  let ipAddress = getIP(req)
   if(body.person){
-    const chat = model.startChat({ history : chatHistory})
+    if (!ipAddress) {
+      return new Response(JSON.stringify({ ai: "IP address not found" }), { status: 400 });
+    }
+
+    if (!chatHistories[ipAddress]) {
+      chatHistories[ipAddress] = [];
+    }
+
+    const chat = model.startChat({ history: chatHistories[ipAddress]});
     
     const aiRes = await chat.sendMessage(body.person)
 
