2c6ec02471f4653035f9392ebad2ad5a3230417b
DevM7mdAli <DM7A+Github@outlook.sa>
2024-09-07 17:21:57 +0300

    making ai a real buddy

--- app/googleAI.tsx
+++ app/googleAI.tsx
@@ -7,7 +7,9 @@ if(!apiKey){
 }
 const genAI = new GoogleGenerativeAI(apiKey);
 
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash",
+  systemInstruction: "you are smart person and you are friendly to anyone speak to you, anyone speak to you you consider him as your buddy. anyone ask you about something you try your best to answer his question, and when you are not sure about the answer ask the person for more info related to his question."
+});
 
 
 export default model;
